d4d1d99a8c45a605b137cce644e6cdbdf8c7901a
yu-ratel <dbdudals100@gmail.com>
2024-06-06 19:12:21 +0900

    feat: for-me page data fetch 및 style 조정

--- src/containers/forme/AskList.tsx
+++ src/containers/forme/AskList.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
+'use client';
 
-const datas = [
-  {
-    id: 1,
-    ask: '1년 후 내 모습은 어떨 것 같나요?',
-  },
-  {
-    id: 2,
-    ask: '3년 후 내 모습은 어떨 것 같나요?',
-  },
-  {
-    id: 3,
-    ask: '10년 후 내 모습은 어떨 것 같나요?',
-  },
-  {
-    id: 4,
-    ask: '나의 장점은 무엇인가요?',
-  },
-  {
-    id: 5,
-    ask: '최근 세운 나의 목표가 있나요?',
-  },
-];
+import { useState, useEffect } from 'react';
+import { Database } from '@/types/supabase';
+
+type AskDto = Database['public']['Tables']['questionnaires']['Row'];
 
 const AskList = ({ onClick }: { onClick: (ask: string) => void }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/api/questionnaire');
+      const result = await response.json();
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <section className="m-10 grid h-[28rem] w-[30rem] bg-white">
-      <h1 className="h-20 content-center bg-deepBraun text-center">나를 알아가보세요.</h1>
-      {datas.map((data) => {
-        return (
-          <button type="button" key={data.id} onClick={() => onClick(data.ask)}>
-            {data.ask}
-          </button>
-        );
-      })}
+    <section className="m-10 grid h-[28rem] w-[30rem] overflow-auto bg-white">
+      <h1 className="h-[5.5rem] content-center bg-deepBraun text-center">나를 알아가보세요.</h1>
+      <div className="overflow-auto p-2">
+        {data.map((item: AskDto) => {
+          return (
+            <button
+              className="mb-5 w-full p-5 shadow-md"
+              type="button"
+              key={item.id}
+              onClick={() => onClick(item.question)}
+            >
+              {item.question}
+            </button>
+          );
+        })}
+      </div>
     </section>
   );
 };
 
 export default AskList;
+
+// 오늘 배운점
+// SSR에서 routeHandler를 사용할때는 데이터를 패칭해오는 과정이 필요하다.
+// serverAction이 굳이 필요한가? -> get안됌
+// 에러에 대한 로딩도 공부해야할듯
+// useClinet 로 설정했으니까 상호작용하는 onClick넣어도될듯
--- src/containers/forme/AskReply.tsx
+++ src/containers/forme/AskReply.tsx
@@ -15,14 +15,14 @@ const AskReply = () => {
   return (
     <>
       <section className=" m-10 h-[28rem] w-[55rem] bg-white">
-        <header className="h-24 bg-deepBraun">
+        <header className="h-1/5 bg-deepBraun">
           <div className="flex h-full items-center justify-evenly">
             <h3>{text}</h3>
             <div className=" text-gray">{today}</div>
           </div>
         </header>
-        <form>
-          <input type="text" placeholder="Some text" />
+        <form className="h-4/5">
+          <textarea className="h-full w-full " placeholder="Some text" />
         </form>
       </section>
       <AskList onClick={askClick} />
