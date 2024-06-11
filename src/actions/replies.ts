e47bc78bc45c1186a627568d972338992844fe0a
yu-ratel <dbdudals100@gmail.com>
2024-06-11 23:17:36 +0900

    feat: replies(질문 답변) server actions CRUD

---
+++ src/actions/replies.ts
@@ -0,0 +1,55 @@
+'use server';
+
+import { creatServer } from '@/lib/supabase/server';
+
+export const get = async () => {
+  const supabase = await creatServer();
+
+  const result = await supabase.from('user_replies_ris').select('*').is('deleted_at', null);
+
+  return result.data;
+};
+
+export const create = async (question: string, replie: string) => {
+  const supabase = await creatServer();
+
+  const result = await supabase
+    .from('user_replies_ris')
+    .insert({
+      question,
+      replie,
+    })
+    .select();
+
+  return result.data;
+};
+
+export const update = async (id: string, replie: string) => {
+  const supabase = await creatServer();
+
+  const result = await supabase
+    .from('user_replies_ris')
+    .update({
+      replie,
+      updated_at: new Date().toISOString(),
+    })
+    .eq('id', id)
+    .select();
+
+  return result.data;
+};
+
+export const deleted = async (id: string) => {
+  const supabase = await creatServer();
+
+  const result = await supabase
+    .from('user_replies_ris')
+    .update({
+      deleted_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    })
+    .eq('id', id)
+    .select();
+
+  return result.data;
+};
