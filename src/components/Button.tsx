e7e5bb5abb7e3ce6f1dde6ac5f8ff293d24e01fe
yu-ratel <dbdudals100@gmail.com>
2024-07-05 16:45:28 +0900

    feat: button 공통 컴포넌트구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { ButtonHTMLAttributes } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'update';
+}
+
+function Button({ variant = 'primary', children, ...props }: Props) {
+  const STYLE = {
+    primary:
+      'bottom-36 mx-20 h-16  w-44 content-center justify-self-center bg-deepBraun text-center text-white shadow-xl',
+    update: '',
+  };
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <button type="button" className={STYLE[variant]} {...props}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  variant: 'primary',
+};
+
+export default Button;
