e498a707fd2d0a5033a8a53e709acd37bf08d7ac
yu-ratel <dbdudals100@gmail.com>
2024-07-07 19:07:36 +0900

    feat: post page 게시글 data fetch id에 맞는 단일 객체 fetch

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+import creatServer from '@/lib/supabase/server';
+
+export const GET = async (request: Request) => {
+  const { searchParams } = new URL(request.url);
+  const id = parseInt(searchParams.get('id') || '1', 10);
+  const supabase = await creatServer();
+
+  const { data } = await supabase.from('user_replies_ris').select().eq('id', id).single();
+
+  return NextResponse.json(data);
+};
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,12 +1,27 @@
-export default function PostPage() {
+import { Database } from '@/types/supabase';
+
+type BoardDto = Database['public']['Tables']['user_replies_ris']['Row'];
+
+const fetchData = async (id: number) => {
+  const curId = Number(id);
+  const response = await fetch(`http://localhost:3000/api/post?id=${curId}`);
+
+  return response.json();
+};
+
+async function PostPage({ params }: { params: { id: number } }) {
+  const data: BoardDto = await fetchData(params.id);
+
   return (
     <main className="h-[80%]">
       <h1 className="text-center text-4xl text-white">우리들의 이야기</h1>
       <section className="flex h-full">
         <section className="ml-10 mt-10 h-4/5 w-3/5 bg-white *:m-5">
-          <div>제목</div>
-          <div>이름 날짜</div>
-          <div>글</div>
+          <div>{data.question}</div>
+          <div>
+            {data.nickname} {data.updated_at}
+          </div>
+          <div>{data.replie}</div>
         </section>
         <section className="ml-2 mr-10 mt-10 h-4/5 w-2/5 bg-white *:m-5">
           <div>댓글창</div>
@@ -16,3 +31,5 @@ export default function PostPage() {
     </main>
   );
 }
+
+export default PostPage;
