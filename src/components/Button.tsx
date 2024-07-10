8a8e02c811cd8e07af7e2914e33b289bd25788fc
yu-ratel <dbdudals100@gmail.com>
2024-07-10 18:02:11 +0900

    style: 공통 버튼, 댓글창 UI 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 function Button({ variant = 'primary', children, ...props }: Props) {
   const STYLE = {
     primary:
-      'bottom-36 mx-20 h-16  w-44 content-center justify-self-center bg-deepBraun text-center text-white shadow-xl',
+      'bottom-36 mx-20 h-16 rounded-xl w-44 content-center justify-self-center bg-deepBraun text-center text-white shadow-xl',
     update: '',
   };
   return (
--- src/containers/post/Comment.tsx
+++ src/containers/post/Comment.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import { FaRegPenToSquare as Pen } from 'react-icons/fa6';
 
+import Button from '@/components/Button';
+
 function Comment() {
   const [isCommentWindow, SetCommentWindow] = useState(false);
 
@@ -10,29 +12,36 @@ function Comment() {
     SetCommentWindow(true);
   };
 
+  const closeCommentWindow = () => {
+    SetCommentWindow(false);
+  };
+
   return (
-    <section className="ml-2 mr-10 mt-10 h-4/5 w-2/5 bg-white *:m-2 *:h-10">
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div>댓글</div>
-      <div className="flex items-center  rounded-md  border border-solid p-1.5">
-        <Pen className="text-z mr-3" />
-        <input
-          type="text"
-          className="w-full"
-          placeholder="댓글을 작성해보세요."
-          onClick={onCommentWindow}
-        />
-        {isCommentWindow && (
-          <div className="border-gray-300 duration-350 ease absolute  bottom-0 right-[30px] z-[5] h-[200px] w-[38%] overflow-hidden rounded border bg-white transition-all">
-            ㅎㅇ
-          </div>
-        )}
+    <section className="ml-2 mr-10 mt-10 h-4/5 w-2/5 bg-white">
+      <div className="h-4/5 *:h-16">
+        <div>댓글1</div>
+        <div>댓글2</div>
+        <div>댓글3</div>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex w-4/5 items-center rounded-md  border border-solid p-1.5">
+          <Pen className="mr-3 text-3xl" />
+          <input
+            type="text"
+            className="w-full"
+            placeholder="댓글을 작성해보세요."
+            onClick={onCommentWindow}
+          />
+          {isCommentWindow && (
+            <div className="border-gray-300 absolute right-[30px] z-[5] h-[200px] w-[40%] overflow-hidden rounded border bg-braun p-2.5">
+              <textarea className=" mt-3 h-20 w-full resize-none rounded-xl" />
+              <div className="flex justify-center bg-braun *:m-5">
+                <Button onClick={closeCommentWindow}>작성</Button>
+                <Button onClick={closeCommentWindow}>닫기</Button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
