6d7c2ce6277b7941345bcfe1e5a95ab99d6545b7
yu-ratel <dbdudals100@gmail.com>
2024-07-15 23:49:11 +0900

    style: Noto_Serif_KR font 설정(200)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Noto_Serif_KR } from 'next/font/google';
+
 import Nav from '@/components/Nav';
 
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+
+const notoSerifKr = Noto_Serif_KR({
+  subsets: ['latin'],
+  weight: '200',
+});
 
 export const metadata: Metadata = {
   title: 'beauty',
@@ -15,7 +22,7 @@ function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body>
+      <body className={notoSerifKr.className}>
         <Nav />
         {children}
       </body>
