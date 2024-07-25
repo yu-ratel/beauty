a8793247a18538587f157f0fc50a6e479337b40f
yu-ratel <dbdudals100@gmail.com>
2024-07-25 21:43:13 +0900

    refactor: boardPost Api 조건 추가 헤더로 user_id가 들어올 시 해당하는 게시물, 갯수 return

--- src/app/api/boardPost/route.ts
+++ src/app/api/boardPost/route.ts
@@ -7,20 +7,39 @@ export const GET = async (request: Request) => {
   // 쿼리스트링(기본값 1) 받아오면서 -1 (0부터 인덱스 시작)
   const page = parseInt(searchParams.get('page') || '1', 10) - 1;
   const supabase = await creatServer();
+  const userId = request.headers.get('user_id');
+
   const limit = 6;
   // ex) page = 1 limit = 2 =>  0 - 1  page = 2 limit = 2 => 2 - 3
   const [startNum, endNum] = [page * limit, (page + 1) * limit - 1];
 
-  const totalCount = await supabase
-    .from('user_post_rls')
-    .select('*', { count: 'exact', head: true })
-    .is('deleted_at', null);
-  const data = await supabase
-    .from('user_post_rls')
-    .select('*')
-    .is('deleted_at', null)
-    .order('id', { ascending: true })
-    .range(startNum, endNum);
+  const getTotalCount = () => {
+    const data = supabase
+      .from('user_post_rls')
+      .select('*', { count: 'exact', head: true })
+      .is('deleted_at', null);
 
-  return NextResponse.json({ totalCount: totalCount.count, ...data, limit });
+    if (userId) data.eq('user_id', userId);
+
+    return data;
+  };
+
+  const getData = () => {
+    const data = supabase
+      .from('user_post_rls')
+      .select('*')
+      .is('deleted_at', null)
+      .order('id', { ascending: true })
+      .range(startNum, endNum);
+
+    if (userId) data.eq('user_id', userId);
+
+    return data;
+  };
+
+  const totalCountData = await getTotalCount();
+  const data = await getData();
+  const totalCount = totalCountData.count;
+
+  return NextResponse.json({ totalCount, ...data, limit });
 };
