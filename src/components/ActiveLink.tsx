4add9484860544647d4239bb8426ea724c7bd4af
yu-ratel <dbdudals100@gmail.com>
2024-07-26 18:20:57 +0900

    fix: ActiveLink mypage에서 지속적으로 적용 안되는 문제 해결 isActive 함수 조건 추가 (매개변수 활용)

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -7,12 +7,18 @@ import { AnchorHTMLAttributes } from 'react';
 
 interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
   path: string;
+  myPage?: boolean;
 }
 
-function ActiveLink({ path, children, ...props }: Props) {
+function ActiveLink({ path, myPage, children, ...props }: Props) {
   const router = usePathname();
 
-  const isActive = (curPath: string) => router === curPath;
+  const isActive = (curPath: string) => {
+    if (myPage && router.includes('mypage')) return true;
+
+    return router === curPath;
+  };
+
   return (
     <Link href={path} className={isActive(path) ? 'text-white' : 'text-black'} {...props}>
       {children}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,7 +11,11 @@ function Nav() {
       <ActiveLink path="/">아름다움</ActiveLink>
       <div className="*:m-6">
         <ActiveLink path="/board/1">게시판</ActiveLink>
-        {isUser && <ActiveLink path="/mypage/info">마이페이지</ActiveLink>}
+        {isUser && (
+          <ActiveLink path="/mypage/info" myPage>
+            마이페이지
+          </ActiveLink>
+        )}
         <ActiveLink path={`/auth?${isUser}`}>{loginText}</ActiveLink>
       </div>
     </nav>
