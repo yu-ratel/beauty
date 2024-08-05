29460224c82b5bcf4198f81b89a14e0569c9fa16
yu-ratel <dbdudals100@gmail.com>
2024-08-06 02:57:53 +0900

    fix: supabase server에서의 쿠키조정으로 인한 middleware 오류 해결 사유: setting up serverSide 를 middleware 전용이 아닌 serverClient로 사용하여 쿠키설정 -> next는 action, route를 제외한 쿠키설정이 안되어서 페이지오류 해결: middleware 전용 supabase setting 및 getUser를 이용한 사용자 유효성검사 및 mypage접근 불가까지 설정

---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,25 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextRequest } from 'next/server';
+
+function createMiddleware(request: NextRequest) {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+      },
+    },
+  );
+}
+
+export default createMiddleware;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import creatServer from './lib/supabase/server';
+import createMiddleware from './lib/supabase/middleware';
 
 async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  const supabase = await creatServer();
-  const { data, error } = await supabase.auth.getSession();
+  const supabase = createMiddleware(request);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  requestHeaders.set('isUser', data.session ? 'true' : 'false');
-
-  if (error) request.headers.set('isUser', 'false');
+  requestHeaders.set('isUser', 'true');
+  if (!user) {
+    requestHeaders.set('isUser', 'false');
+    if (request.nextUrl.pathname.startsWith('/mypage')) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/`);
+    }
+  }
 
   return NextResponse.next({
     request: {
