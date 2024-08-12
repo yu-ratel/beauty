c6a05772be6c7127494f51976006a87f0c6a63e4
yu-ratel <dbdudals100@gmail.com>
2024-08-12 16:49:46 +0900

    fix: 배포 실패 오류 디버깅 eslint array index key 금지 무효화

--- src/components/SkeletonBoard.tsx
+++ src/components/SkeletonBoard.tsx
@@ -3,6 +3,7 @@ function SkeletonBoard() {
     <>
       <div className="h-[10%] w-full bg-gray " />
       {Array.from({ length: 5 }).map((_, index) => (
+        // eslint-disable-next-line react/no-array-index-key
         <ol className="flex *:m-6 *:h-5 *:animate-pulse *:rounded *:bg-gray" key={index}>
           <li className="w-[10%]" />
           <li className="w-[50%]" />
